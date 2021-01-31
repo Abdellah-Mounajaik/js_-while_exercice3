fe98a18307c32398e2207b306dc94cf97bb032d5
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 01:39:16 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,100 @@
+// exo1
+
+// let compt = 0
+// let prenom = ["abdel", "pomme", "fraise", "poire", "orange", "tomate", "oignon", "melon", "banane" ]
+// while (compt < prenom.length ) {
+//     console.log(`bonjour ${prenom}`);
+//     compt++
+// }
+// do {
+//     let prenom = ["abdel", "pomme", "fraise", "poire", "orange", "tomate", "oignon", "melon", "banane" ]
+//     console.log(`bonjour ${prenom}`);
+//     compt++
+// } while (compt < 9);
+
+// exo2
+
+
+// let fruits = ["pomme", "fraise", "poire", "orange", "tomate", "melon" ]
+// while (fruits.length > 0) {
+//     fruits.pop()
+//     console.log(fruits);
+//     console.log(`Le tableau a ${fruits.length} elements`);
+//     // fruits++
+// }
+    let fruits = ["pomme", "fraise", "poire", "orange", "tomate", "melon" ]
+
+// do {
+//     fruits.pop()
+//     console.log(fruits);
+//     console.log(`Le tableau a ${fruits.length} elements`);
+// } while (fruits.length > 0) {
+//     console.log(fruits);
+// }
+
+
+// exo3
+
+     let panierLegumes = ["carotte", "oignon","concombre", "brocoli"]
+     let caisseLegumes = []
+     var longueur = panierLegumes.length
+     let compt = 0;
+//     while (compt <= longueur) {
+//     panierLegumes.shift()
+//     caisseLegumes.push(panierLegumes[0])
+//     console.log(panierLegumes);
+//     compt++
+// }
+
+
+// do {
+//         console.log(panierLegumes);
+//         caisseLegumes.push(panierLegumes[0])
+//         panierLegumes.splice(0, 1)
+//         var longueur = panierLegumes.length;
+    
+//         compt--
+//     } while (longueur > 0);
+//     console.log(panierLegumes);
+
+// exo4
+// let question = prompt("couleur préféré?")
+// while (question !== "bleu") {
+//     question = prompt("faux, re tente, couleur préféré?")
+// }
+// alert("felicitation")
+
+
+// exo5
+
+// do {
+//     var question = prompt("couleur préféré?")
+//     if (question != "bleu") {
+//         question = prompt("couleur préféré?")
+//     } else {
+//     alert("felicitation")
+//     }
+    
+// } while (question != "bleu");
+
+// exo6
+
+// let actuelle = 2020
+// let annee = 1970
+//     let question = prompt("choisis")    
+
+// for (let i = 0; i < 2; i++) {
+//     if (question != "juste")  {
+//         question = prompt("choisis")
+//     } else {
+        
+//     }
+// }
+
+// Créer une variable avec l'année actuelle
+// Créer un programme qui génère une année de façon aléatoire entre 1970 et 2018. L’utilisateur doit donner le bon age en répondant à la question suivante
+// : “Si je suis née en 1988(année généré aléatoirement), quel age ai-je aujourd’hui ? ”
+// L’utilisateur a trois essais. S’il donne la bonne réponse, on sort et s’il arrive à trois essaies on sort également.
+// Finir avec un affichage.
+
+
